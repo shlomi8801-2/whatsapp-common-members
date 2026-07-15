4265b239739e572fe3562bbe0484e26b48217039
shlomi <shlomi8801@gmail.com>
2026-07-15 14:50:59 +0300

    able to send messages after connection

--- index.js
+++ index.js
@@ -1,46 +1,68 @@
-//credit https://whiskeysockets-baileys-94.mintlify.app/quickstart
-import {makeWASocket, useMultiFileAuthState, DisconnectReason } from 'baileys';
+import { makeWASocket, useMultiFileAuthState, DisconnectReason } from "baileys";
 import QRCode from "qrcode";
-async function connectToWhatsApp(){
-    const { state, saveCreds } = await useMultiFileAuthState('auth');
+import P from "pino";
+import { sleep } from "@cacheable/utils";
+
+var connectionState = 2; // 'open' 0 | 'connecting' 1 | 'close' 2
+
+async function connectToWhatsApp(tries = 5) {
+    if(tries<=0){
+        return null
+    }
+  const { state, saveCreds } = await useMultiFileAuthState("auth");
   const sock = makeWASocket({
     auth: state,
     browser: ["Firefox (Linux)", "", ""],
-
+    syncFullHistory: false,
+    markOnlineOnConnect: false,
   });
 
-  sock.ev.on('creds.update', saveCreds);
-
-  sock.ev.on('connection.update', async (update) => {
-  const {connection, lastDisconnect, qr } = update
-  // on a qr event, the connection and lastDisconnect fields will be empty
-
-  // In prod, send this string to your frontend then generate the QR there
-  if (qr) {
-    // as an example, this prints the qr code to the terminal
-    console.log(await QRCode.toString(qr, {type:'terminal',small:true}))
-  }
-})
-return sock;
-}
-(async () => {
+  sock.ev.on("creds.update", saveCreds); // save the login data with saveCreds function
   
-const sock = await connectToWhatsApp();
-
-sock.ev.on('connection.update', (update) => {
-  const { connection, lastDisconnect } = update
-  
-  if (connection === 'close') {
-    const shouldReconnect = (lastDisconnect?.error)?.output?.statusCode !== DisconnectReason.loggedOut
-    console.log('connection closed due to', lastDisconnect?.error, ', reconnecting:', shouldReconnect)
-    
-    // Reconnect if not logged out
-    if (shouldReconnect) {
-      connectToWhatsApp()
+  sock.ev.on("connection.update", async (update) => {
+    const { connection, lastDisconnect, qr } = update;
+    if (qr) {
+      console.log(await QRCode.toString(qr, { type: "terminal", small: true }));
     }
-  } else if (connection === 'open') {
-    console.log('opened connection')
-  }
-})
+  });
 
+  sock.ev.on("connection.update", (update) => {
+    const { connection, lastDisconnect } = update;
+
+    if (connection === "close") {
+      const shouldReconnect =
+        lastDisconnect?.error?.output?.statusCode !==
+        DisconnectReason.loggedOut;
+      console.log(
+        "connection closed due to",
+        lastDisconnect?.error,
+        ", reconnecting:",
+        shouldReconnect,
+      );
+
+      // Reconnect if not logged out
+      if (shouldReconnect) {
+        return connectToWhatsApp(tries-1);
+      }
+    } else if (connection === "open") {
+      console.log("opened connection");
+      connectionState = 0; 
+    }
+  });
+  
+  return sock;
+}
+function buildjid(phoneNumber, countryCode = "972", isDm = true) {
+  return `${countryCode}${phoneNumber}@${isDm ? "s.whatsapp.net" : "g.us"}`;
+}
+
+(async () => {
+    const sock =await connectToWhatsApp();
+    while(connectionState!=0){
+        await sleep(200);
+    }
+if(sock)
+for (let x=5;x>0;x--)
+sock.sendMessage(buildjid("500000000"),{"text":"yes"});
 })();
+
